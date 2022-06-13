f1e8381622cf0a966ce5993345eb8af97a11930e
Alexandr <caiiiia_podoplelov@mail.ru>
2022-06-13 23:12:41 +0300

    queen moves

--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -9,4 +9,20 @@ export class Queen extends Figure {
     this.name = FigureNames.QUEEN;
     this.pieceId = color === Colors.BLACK ? 'blackQueen' : 'whiteQueen';
   };
+
+
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target))
+      return false;
+
+    if (
+        this.cell.isEmptyVertical(target) ||
+        this.cell.isEmptyHorizontal(target) ||
+        this.cell.isEmptyDiagonal(target)
+      )
+      return true;
+
+
+    return false;
+  };
 };
